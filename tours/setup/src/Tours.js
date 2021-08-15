ec602235b7a4f41af36709b852bc348c6edc6751
rendinel <rendinel@protonmail.com>
2021-08-15 15:37:53 +0200

    push

---
+++ tours/setup/src/Tours.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Tour from './Tour'
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className='title'>
+        <h2>Ours Tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour removeTour={removeTour} key={tour.id} {...tour}></Tour>
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Tours
